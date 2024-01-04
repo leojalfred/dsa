98aa58932a5eb8e527b7587aa3a3cbf28865138e
Leo Alfred <leojalfred@gmail.com>
2024-01-03 20:17:24 -0600

    Create MyStringBuilder

---
+++ src/MyStringBuilder.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MyStringBuilder {
+    ArrayList<String> strings = new ArrayList<>();
+
+    public void append(String string) {
+        strings.add(string);
+    }
+
+    public String toString() {
+        char[] characters = new char[100];
+        int size = 0;
+
+        for (String string: strings) {
+            for (int i = 0; i < string.length(); i++) {
+                if (size == characters.length)
+                    characters = Arrays.copyOf(characters, 2 * characters.length);
+
+                characters[size] = string.charAt(i);
+                size++;
+            }
+        }
+
+        return new String(characters);
+    }
+}
