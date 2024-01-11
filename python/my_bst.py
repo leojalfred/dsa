3eb09c1e824ccbda3188f042627d56d78de1521d
Leo Alfred <leojalfred@gmail.com>
2024-01-10 18:44:24 -0600

    Implement BST traversal

---
+++ python/my_bst.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+from typing import Callable, Any
+
+
+class Node[T]:
+    left: Node[T] = None
+    right: Node[T] = None
+
+    def __init__(self, data: T) -> None:
+        self.data = data
+
+
+def pre_order(node: Node, visit: Callable[[Any], None]) -> None:
+    if node is not None:
+        visit(node.data)
+        pre_order(node.left, visit)
+        pre_order(node.right, visit)
+
+
+def in_order(node: Node, visit: Callable[[Any], None]) -> None:
+    if node is not None:
+        in_order(node.left, visit)
+        visit(node.data)
+        in_order(node.right, visit)
+
+
+def post_order(node: Node, visit: Callable[[Any], None]) -> None:
+    if node is not None:
+        post_order(node.left, visit)
+        post_order(node.right, visit)
+        visit(node.data)
+
+
+a = Node(1)
+b = Node(2)
+c = Node(3)
+d = Node(4)
+e = Node(5)
+f = Node(6)
+g = Node(7)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.left = f
+c.right = g
+
+print('Pre-Order:')
+pre_order(a, print)
+
+print('\nIn-Order:')
+in_order(a, print)
+
+print('\nPost-Order:')
+post_order(a, print)
