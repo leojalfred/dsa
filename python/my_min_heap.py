1f444ba37201c819ec27a49d2eaaaf1d4d3b83a4
Leo Alfred <leojalfred@gmail.com>
2024-01-13 18:18:47 -0600

    Refactor min heap

--- python/my_min_heap.py
+++ python/my_min_heap.py
@@ -1,58 +1,69 @@
-class MinHeap:
-    def __init__(self):
+class MinHeap[T]:
+    def __init__(self) -> None:
         self.heap = [0]
         self.size = 0
+        self.TOP = 1
 
-    def bubble_up(self, i):
-        while i // 2 > 0 and self.heap[i] < self.heap[i // 2]:
-            self.heap[i], self.heap[i // 2] = self.heap[i // 2], self.heap[i]
-            i = i // 2
+    @staticmethod
+    def parent(i: int) -> int:
+        return i // 2
 
-    def insert(self, k):
-        self.heap.append(k)
+    @staticmethod
+    def left(i: int) -> int:
+        return i * 2
+
+    @staticmethod
+    def right(i: int) -> int:
+        return i * 2 + 1
+
+    def bubble_up(self, i: int) -> None:
+        while self.parent(i) > 0 and self.heap[i] < self.heap[self.parent(i)]:
+            self.heap[i], self.heap[self.parent(i)] = self.heap[self.parent(i)], self.heap[i]
+            i = self.parent(i)
+
+    def insert(self, element: T) -> None:
+        self.heap.append(element)
         self.size += 1
         self.bubble_up(self.size)
 
-    def bubble_down(self, i):
-        while (i * 2) <= self.size:
-            mc = self.min_child(i)
-
-            if self.heap[i] > self.heap[mc]:
-                self.heap[i], self.heap[mc] = self.heap[mc], self.heap[i]
-
-            i = mc
-
-    def min_child(self, i):
-        if (i * 2) + 1 > self.size:
-            return i * 2
-        else:
-            if self.heap[i * 2] < self.heap[(i * 2) + 1]:
-                return i * 2
+    def bubble_down(self, i: int) -> None:
+        while self.left(i) <= self.size:
+            if self.right(i) > self.size:
+                min_child = self.left(i)
             else:
-                return (i * 2) + 1
+                if self.heap[self.left(i)] < self.heap[self.right(i)]:
+                    min_child = self.left(i)
+                else:
+                    min_child = self.right(i)
 
-    def extract_min(self):
+            if self.heap[i] > self.heap[min_child]:
+                self.heap[i], self.heap[min_child] = self.heap[min_child], self.heap[i]
+
+            i = min_child
+
+    def extract_min(self) -> T:
         if len(self.heap) == 1:
             raise IndexError
 
-        root = self.heap[1]
+        minimum = self.heap[self.TOP]
 
-        self.heap[1] = self.heap[self.size]
+        self.heap[self.TOP] = self.heap[self.size]
         *self.heap, _ = self.heap
         self.size -= 1
         self.bubble_down(1)
 
-        return root
+        return minimum
 
 
 if __name__ == '__main__':
     my_heap = MinHeap()
-    my_heap.insert(5)
-    my_heap.insert(6)
-    my_heap.insert(7)
+    my_heap.insert(-213)
+    my_heap.insert(1)
+    my_heap.insert(-28467)
+    my_heap.insert(123)
     my_heap.insert(9)
-    my_heap.insert(13)
-    my_heap.insert(11)
-    my_heap.insert(10)
+    my_heap.insert(0)
+    my_heap.insert(8737)
 
-    print(my_heap.extract_min())  # removing min node i.e 5
+    for _ in range(my_heap.size):
+        print(my_heap.extract_min())
